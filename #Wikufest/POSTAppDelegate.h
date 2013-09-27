1932e9670321973ee0584af6e9f79c64da410b09
Arie <arie@wego.com>
2013-09-27 11:03:14 +0700

    Initial Commit

---
+++ #Wikufest/POSTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  POSTAppDelegate.h
+//  #Wikufest
+//
+//  Created by Arie on 9/27/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface POSTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ #Wikufest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  #Wikufest
+//
+//  Created by Arie on 9/27/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "POSTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([POSTAppDelegate class]));
+	}
+}
